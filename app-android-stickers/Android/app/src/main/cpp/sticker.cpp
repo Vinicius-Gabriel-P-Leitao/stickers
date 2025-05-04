2f5e34ab1bb3f92ef0859b68c77d3434944eb7e1
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2025-05-04 22:40:54 +0000

    [feat]: Configura gradle para empacotar as libs c++
    Feito configurção para ele filtrar baseado na abi.

--- app-android-stickers/Android/app/src/main/cpp/sticker.cpp
+++ app-android-stickers/Android/app/src/main/cpp/sticker.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <android/log.h>
+#include <iostream>
 
 extern "C" {
 #include <libavformat/avformat.h>
@@ -7,6 +8,8 @@ extern "C" {
 #include <libavutil/avutil.h>
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
+#include <encode.h>
+#include <decode.h>
 }
 
 extern "C" JNIEXPORT jstring JNICALL
@@ -16,8 +19,23 @@ Java_com_vinicius_sticker_domain_libs_ConvertToWebp_convertToWebp(JNIEnv *env, j
     const char *inPath = env->GetStringUTFChars(inputPath, nullptr);
     const char *outPath = env->GetStringUTFChars(outputPath, nullptr);
 
-    unsigned int version = avformat_version();
-    char version_str[32];
-    snprintf(version_str, sizeof(version_str), "libavformat version: %u", version);
+    if (inPath == nullptr || outPath == nullptr) {
+        if (inPath) env->ReleaseStringUTFChars(inputPath, inPath);
+        if (outPath) env->ReleaseStringUTFChars(outputPath, outPath);
+        return env->NewStringUTF("Invalid input or output path");
+    }
+
+    unsigned int versionFfmpeg = avformat_version();
+    char version_str[64];
+    snprintf(version_str, sizeof(version_str), ""
+                   "\n libavformat version: %u", versionFfmpeg);
+
+    unsigned int webp_version = WebPGetDecoderVersion();
+    snprintf(version_str + strlen(version_str), sizeof(version_str) - strlen(version_str),
+             "\n libwebp version: %u\n", webp_version);
+
+    env->ReleaseStringUTFChars(inputPath, inPath);
+    env->ReleaseStringUTFChars(outputPath, outPath);
+
     return env->NewStringUTF(version_str);
 }
